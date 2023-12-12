c5d1c377e334d96ba94e1c1ead3175a9b42ea942
alxplv <polishchuk-alexander@mail.ru>
2023-12-12 22:07:04 +0600

    Update layout to support all mod_articles_news module  params

--- html/mod_articles_news/airis_item.php
+++ html/mod_articles_news/airis_item.php
@@ -92,14 +92,18 @@ if ($params->get('img_intro_full') !== 'none' && isset($item->imageSrc) && $item
         <?php endif; ?>
 
         <?php if ($params->get('item_title')) : ?>
-            <div class="airis-module-articles-news__">
+            <div class="airis-module-articles-news__title-container airis-module-header airis-header">
                 <?php
                     $itemHeadingTag = $params->get('item_heading', 'h4');
 
-                    echo "<$itemHeadingTag class=\"airis-module-articles-news-article-title-heading\">";
+                    echo "<$itemHeadingTag class=\"airis-module-articles-news__title\">";
 
                     if ($params->get('link_titles') && isset($item->link) && $item->link !== '') {
-                        echo HTMLHelper::link($item->link, $item->title);
+                        $titleLinkAttributes = [
+                            'class' => 'airis-module-articles-news__title-link',
+                        ];
+
+                        echo HTMLHelper::link($item->link, $item->title, $titleLinkAttributes);
                     } else {
                         echo $item->title;
                     }
@@ -109,34 +113,66 @@ if ($params->get('img_intro_full') !== 'none' && isset($item->imageSrc) && $item
             </div>
         <?php endif; ?>
 
-        <?php if (!$params->get('intro_only')) : ?>
-            <div class="airis-module-articles-news-article-title-after">
+        <?php if (!$params->get('intro_only') && isset($item->afterDisplayTitle) && $item->afterDisplayTitle !== '') : ?>
+
+            <div class="airis-module-articles-news__title-after airis-item-content">
                 <?php echo $item->afterDisplayTitle; ?>
             </div>
+
         <?php endif; ?>
 
-        <?php if ($params->get('show_introtext', true)) : ?>
-            <div class="airis-module-articles-news-article-content airis-item-content">
+        <?php if (isset($item->beforeDisplayContent) && $item->beforeDisplayContent !== '') : ?>
+
+            <div class="airis-module-articles-news__content-before airis-item-content">
+                <?php echo $item->beforeDisplayContent; ?>
+            </div>
+
+        <?php endif; ?>
+
+        <?php if ($params->get('show_introtext') && isset($item->introtext) && $item->introtext !== '') : ?>
+
+            <div class="airis-module-articles-news__content airis-item-content">
                 <?php echo $item->introtext; ?>
             </div>
+
         <?php endif; ?>
 
-        <div class="airis-module-articles-news-article-content-after">
-            <?php echo $item->afterDisplayContent; ?>
-        </div>
+        <?php if (isset($item->afterDisplayContent) && $item->afterDisplayContent !== '') : ?>
 
-        <?php if ($item->link && $item->readmore && $params->get('readmore')) : ?>
-            <div class="airis-module-articles-news-article-readmore">
+            <div class="airis-module-articles-news__content-after airis-item-content">
+                <?php echo $item->afterDisplayContent; ?>
+            </div>
+
+        <?php endif; ?>
+
+        <?php if ($params->get('readmore') && $item->link && $item->readmore !== '') : ?>
+
+            <div class="airis-module-articles-news__readmore-container">
                 <?php
+                    $readmoreLinkAttributes = [
+                        'class' => 'airis-module-articles-news__readmore-link btn',
+                    ];
+
+                    // TODO: To be used on Joomla! 4+ with an overriden readmore layout which accepts link attributes and link text
+/*                     echo LayoutHelper::render(
+                        'joomla.content.readmore',
+                        [
+                            'item' => $item,
+                            'params' => $item->params,
+                            'link' => $item->link,
+                            'text' => $item->linkText,
+                            'attributes' => $readmoreLinkAttributes, // TODO: Unset possible $readmoreLinkAttributes['href'] in readmore layout so it doesn't double
+                        ]
+                    ); */
+
                     echo HTMLHelper::link(
                         $item->link,
                         htmlspecialchars(trim($item->linkText), ENT_QUOTES, 'UTF-8'),
-                        [
-                            'class' => 'airis-module-articles-news-article-readmore-link btn',
-                        ],
+                        $readmoreLinkAttributes,
                     );
                 ?>
             </div>
+
         <?php endif; ?>
 
     </article>
