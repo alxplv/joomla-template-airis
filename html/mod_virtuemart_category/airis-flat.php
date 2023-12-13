1055f2ad57f65d68a9934b43629f8c915282dd30
alxplv <polishchuk-alexander@mail.ru>
2023-12-14 00:39:17 +0600

    Update layout to use list instead of divs

--- html/mod_virtuemart_category/airis-flat.php
+++ html/mod_virtuemart_category/airis-flat.php
@@ -5,16 +5,53 @@ defined('_JEXEC') or exit;
 
 // Joomla! imports
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+// Process module data and options
+$menuClassSuffix = airisPrepareHTMLClassSuffix($params->get('class_sfx', ''));
+$moduleClassSuffix = airisPrepareHTMLClassSuffix($params->get('moduleclass_sfx', ''));
+// TODO: Replace is_array() && instancof Countable with is_countable() once we're on PHP 7.3+ for good
+$moduleHasCategories = isset($categories) && is_array($categories) && $categories instanceof Countable && count($categories);
+
+function airisPrepareHTMLClassSuffix(string $classSuffix)
+{
+    // Not using trim() here to keep possible and perfectly acceptable leading whitespace
+    $classSuffix = htmlspecialchars(rtrim($classSuffix), ENT_QUOTES, 'UTF-8');
+
+    // Remove non-singular whitespaces
+    $classSuffix = preg_replace('/\s{2,}/', ' ', $classSuffix);
+
+    return $classSuffix;
+}
 
 ?>
 
-<div class="airis-module-menu<?php echo $class_sfx; ?> virtuemart-module-category<?php echo $class_sfx; ?>">
-	<?php foreach ($categories as $category) : ?>
-		<div class="airis-module-menu-item virtuemart-module-category-item">
-			<?php
-				$categoryHref = 'index.php?option=com_virtuemart&view=category&virtuemart_category_id=' . $category->virtuemart_category_id;
-				echo HTMLHelper::link($categoryHref, htmlspecialchars(trim(vmText::_($category->category_name))), array('class' => 'airis-module-menu-item-link virtuemart-module-category-item-link'));
-			?>
-		</div>
-	<?php endforeach; ?>
+<div class="airis-module-virtuemart-category-flat<?php echo $moduleClassSuffix; ?>">
+
+    <?php if ($moduleHasCategories) : ?>
+
+        <ul class="airis-module-virtuemart-category-flat__list<?php echo $menuClassSuffix; ?> unstyled">
+            <?php foreach ($categories as $category) : ?>
+                <li class="airis-module-virtuemart-category-flat__item">
+                    <?php
+                        echo HTMLHelper::link(
+                            'index.php?option=com_virtuemart&view=category&virtuemart_category_id=' . $category->virtuemart_category_id,
+                            htmlspecialchars(trim(vmText::_($category->category_name)), ENT_QUOTES, 'UTF-8'),
+                            ['class' => 'airis-module-virtuemart-category-flat__link'],
+                        );
+                    ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+    <?php else : ?>
+
+        <div class="airis-module-virtuemart-category-flat-empty airis-module-empty" data-nosnippet>
+            <p class="airis-module-virtuemart-category-flat-empty__message airis-module-empty__message">
+                <?php echo Text::_('TPL_AIRIS_MOD_VIRTUEMART_CATEGORY_NO_CATEGORIES'); ?>
+            </p>
+        </div>
+
+    <?php endif; ?>
+
 </div>
