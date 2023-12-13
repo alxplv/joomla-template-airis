9da2da903b90d0a2a8a1899403afb1822bb17c05
alxplv <polishchuk-alexander@mail.ru>
2023-12-13 11:50:28 +0600

    Add new mod_menu layout file

---
+++ html/mod_menu/airis-bootstrap-collapse.php
@@ -0,0 +1,6 @@
+<?php
+
+// No direct access to this file outside of Joomla!
+defined('_JEXEC') or exit;
+
+// TODO: Create our own version of collapsible in a similar fashion to /modules/mod_menu/tmpl/collapse-default.php
