d5cb540204f9c4d083317323531b0e2b561f1ec1
alxplv <polishchuk-alexander@mail.ru>
2023-12-20 11:40:23 +0600

    J!4-ready code overhaul

--- html/layouts/chromes/airis.php
+++ html/layouts/chromes/airis.php
@@ -3,73 +3,93 @@
 // No direct access to this file outside of Joomla!
 defined('_JEXEC') or exit;
 
-function AirisGetModuleContent($module, &$params, &$attribs, $noSnippetModule = false)
-{
-    // Simple guard statement
-    if (!isset($module->content)) {
-        return;
-    }
+// Joomla! imports
+use Joomla\Utilities\ArrayHelper;
 
-    $moduleHtml = '';
-    $moduleContainerClasses = 'airis-module';
-    $moduleContainerAttributes = '';
-    $moduleHeadingClasses = 'airis-module__heading';
-    $moduleTitle = htmlspecialchars(trim($module->title), ENT_QUOTES, 'UTF-8');
-
-    //
-    // Processing advanced options of the module
-    //
-
-    // Module class suffix
-    $moduleContainerClasses .= htmlspecialchars($params->get('moduleclass_sfx'), ENT_QUOTES, 'UTF-8');
-
-    // Container and heading HTML tag types with addittional class for easy styling
-    $moduleContainerTagType = $params->get('module_tag', 'div');
-    $moduleHeadingTagType = $params->get('header_tag', 'h3');
-    $moduleHeadingClasses .= ' airis-module__heading_level_' . str_replace('h', '', $moduleHeadingTagType);
-
-    // Bootstrap grid size
-    $moduleBootstrapGridSize = $params->get('bootstrap_size', '0');
-    if ($moduleBootstrapGridSize !== '0') {
-        $moduleContainerClasses .= ' span' . $moduleBootstrapGridSize;
-    }
-
-    // Heading classes
-    $moduleHeadingClassesExtra = htmlspecialchars(trim($params->get('header_class', '')), ENT_QUOTES, 'UTF-8');
-    if ($moduleHeadingClassesExtra) {
-        $moduleHeadingClasses .= ' ' . $moduleHeadingClassesExtra;
-    }
-
-    // Stop the contents of this module from appearing in search engine results page descriptions if required. Search engines declare that only <div>, <section> and <span> elements are a valid target for this attribute.
-    if ($noSnippetModule && in_array($moduleContainerTagType, ['div', 'section'])) {
-        $moduleContainerAttributes = $noSnippetModule ? ' data-nosnippet' : '';
-    }
-
-    // Populate module markup with processed options data
-    $moduleHtml .= "<$moduleContainerTagType class=\"$moduleContainerClasses\"$moduleContainerAttributes>";
-
-    if ($module->showtitle) {
-        $moduleHtml .= '<div class="airis-module__header">';
-        $moduleHtml .= "<$moduleHeadingTagType class=\"$moduleHeadingClasses\">$moduleTitle</$moduleHeadingTagType>";
-        $moduleHtml .= '</div>';
-    }
-
-    $moduleHtml .= "<div class=\"airis-module__content\">$module->content</div>";
-    $moduleHtml .= "</$moduleContainerTagType>";
-
-    return $moduleHtml;
+// Skip empty modules
+if ($displayData['module']->content === '') {
+    return;
 }
 
-function modChrome_airis($module, &$params, &$attribs)
-{
-    if (!empty($module->content)) {
-        echo AirisGetModuleContent($module, $params, $attribs);
+// Basic handles
+$module = $displayData['module'];
+$moduleParams = $displayData['params'];
+
+// Prepare module options
+$moduleBootstrapGridSize = $moduleParams->get('bootstrap_size', '0');
+$moduleClassSuffix = htmlspecialchars(rtrim($moduleParams->get('moduleclass_sfx', '')), ENT_QUOTES, 'UTF-8'); // Leftside whitespace is perfectly acceptable
+$moduleClassSuffixFullTrim = ltrim($moduleClassSuffix);
+$moduleContainerElement = $moduleParams->get('module_tag', 'div');
+$moduleHeadingClass = htmlspecialchars(trim($moduleParams->get('header_class', '')), ENT_QUOTES, 'UTF-8');
+$moduleHeadingElement = $moduleParams->get('header_tag', 'h3');
+$moduleHeadingElementCharacters = str_split($moduleHeadingElement);
+$moduleTitle = htmlspecialchars(trim($module->title), ENT_QUOTES, 'UTF-8');
+
+// Prepare HTML class and other attribute strings for our chrome
+$moduleContainerAttributes = [];
+$moduleContainerClasses = "airis-module airis-module_container-type_$moduleContainerElement";
+$moduleHeadingAttributes = [];
+$moduleHeadingClasses = 'airis-module__heading';
+
+// This flag is set when this chrome is invoked by the airis-nosnippet proxy chrome
+if (isset($displayData['airisModuleChromeUseDataNoSnippetAttribute']) && $displayData['airisModuleChromeUseDataNoSnippetAttribute'] === true) {
+    $moduleContainerAttributes['data-nosnippet'] = true;
+}
+
+// "Module Class Suffix" option. If the suffix value has leading whitespace then output it as a separate class instead of a BEM modifier-name_modifier-value pair
+if ($moduleClassSuffix !== '') {
+    if (strlen($moduleClassSuffix) !== strlen($moduleClassSuffixFullTrim)) {
+        $moduleContainerClasses .= " $moduleClassSuffixFullTrim";
+    } else {
+        $moduleContainerClasses .= " airis-module_type_$moduleClassSuffixFullTrim";
     }
 }
 
-function modChrome_airis_nosnippet($module, &$params, &$attribs)
-{
-    if (!empty($module->content)) {
-        echo AirisGetModuleContent($module, $params, $attribs, true);
-    }
+// "Bootstrap Size" option
+if ($moduleBootstrapGridSize !== '0') {
+    $moduleContainerClasses .= " col-md-$moduleBootstrapGridSize";
 }
+
+// "Module Tag" option. The <div> elements without certain aria-roles are not allowed to use the aria-labelledby attribute and we don't use these here
+if ($moduleContainerElement !== 'div') {
+    $moduleIdUniqueSuffix = md5($module->id . hrtime(true)); // The module ID alone wouldn't suffice here as there can be multiple instances of the same module with the same ID per page rendered which would break both the HTML and the ARIA specs
+    $moduleIdHtmlAttribute = "airis-module__heading_unique-id_$moduleIdUniqueSuffix";
+    $moduleContainerAttributes['aria-labelledby'] = $moduleIdHtmlAttribute;
+    $moduleHeadingAttributes['id'] = $moduleIdHtmlAttribute;
+}
+
+// "Header Tag" option. Apply different class sets based on the selected type of the element
+if (count($moduleHeadingElementCharacters) === 2 && $moduleHeadingElementCharacters[0] === 'h' && is_numeric($moduleHeadingElementCharacters[1])) {
+    $moduleHeadingClasses .= " airis-module__heading_type_h airis-module__heading_level_$moduleHeadingElementCharacters[1]";
+} else {
+    $moduleHeadingClasses .= " airis-module__heading_type_$moduleHeadingElement";
+}
+
+// "Header Class" option
+if ($moduleHeadingClass !== '') {
+    $moduleHeadingClasses .= " $moduleHeadingClass";
+}
+
+// Optional whitespace for container and heading HTML attributes output to avoid using static markup whitespace which will lead to unnecessary whitespace in tags without additional attributes set
+$moduleContainerAttributesPadding = (count($moduleContainerAttributes)) ? ' ' : '';
+$moduleHeadingAttributesPadding = (isset($moduleContainerAttributes['aria-labelledby'])) ? ' ' : '';
+
+?>
+
+<<?php echo $moduleContainerElement; ?> class="<?php echo $moduleContainerClasses; ?>"<?php echo $moduleContainerAttributesPadding, ArrayHelper::toString($moduleContainerAttributes); ?>>
+
+    <?php if ($module->showtitle) : ?>
+
+        <div class="airis-module__header">
+            <<?php echo $moduleHeadingElement; ?> class="<?php echo $moduleHeadingClasses; ?>"<?php echo $moduleHeadingAttributesPadding, ArrayHelper::toString($moduleHeadingAttributes); ?>>
+                <?php echo $moduleTitle; ?>
+            </<?php echo $moduleHeadingElement; ?>>
+        </div>
+
+    <?php endif; ?>
+
+    <div class="airis-module__content">
+        <?php echo $module->content; ?>
+    </div>
+
+</<?php echo $moduleContainerElement; ?>>
