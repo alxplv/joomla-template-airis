a9566d6b06f6d6e44f723ea50f988e1140069a81
alxplv <polishchuk-alexander@mail.ru>
2023-12-22 18:59:37 +0600

    Update TODOs

--- html/mod_breadcrumbs/airis.php
+++ html/mod_breadcrumbs/airis.php
@@ -3,4 +3,6 @@
 // No direct access to this file outside of Joomla!
 defined('_JEXEC') or exit;
 
-// TODO: Write a custom implementation for this module view
+// TODO: Write a custom implementation for this module view based on: https://getbootstrap.com/docs/5.3/components/breadcrumb/
+
+// TODO: Add support for Font Awesome SVGs or Web Font glyphs as dividers (described in the BS docu from above) set through airis-module-option- module container classes
