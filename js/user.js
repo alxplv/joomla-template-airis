50c2b4b76b148c0c2e2fce7761ae7bd8cafa31fd
alxplv <polishchuk-alexander@mail.ru>
2024-01-24 19:22:53 +0600

    Add user CSS and JS file templates

---
+++ js/user.js
@@ -0,0 +1,9 @@
+// User JavaScript
+// Please don't forget to enable the use jQuery in template options if you intent to write jQuery-targeted code such as the blocks commented out below
+/* jQuery(function () {
+
+}); */
+
+/* jQuery(window).on("load", function () {
+
+}); */
