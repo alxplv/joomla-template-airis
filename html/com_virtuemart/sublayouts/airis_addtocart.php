c8ee043fb88c78450dfe5e454f32f6bcd096e725
alxplv <polishchuk-alexander@mail.ru>
2024-01-30 19:30:44 +0600

    Update to VM 4.2.4 and refactor for PER 2.0 CS compatibility

--- html/com_virtuemart/sublayouts/airis_addtocart.php
+++ html/com_virtuemart/sublayouts/airis_addtocart.php
@@ -8,24 +8,29 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
 
-if (!isset($viewData['product'])) return;
-$product = $viewData['product'];
+
+if (isset($viewData['product']) === false) {
+	return;
+}
 
 // Used in including proper sublayouts
 $sublayoutPrefix = basename(__FILE__, 'addtocart.php');
 
+// Process product data
+$product = $viewData['product'];
+$productName = htmlspecialchars(trim(vmText::_($product->product_name)), ENT_QUOTES, 'UTF-8');
+$productSku = htmlspecialchars(trim($product->product_sku), ENT_QUOTES, 'UTF-8');
+
 // TODO: Refactor this whole $discaimers deal into a simple array of disclaimers of various types without the need for marker variables and such
 // Controls the output of disclaimers container
-if (isset($viewData['airis-template-options']))
-{
+if (isset($viewData['airis-template-options'])) {
 	$haveDisclaimers = false;
 
 	// Controls the display of not-a-public-offer disclaimer
 	$displayNotAPublicOfferDisclaimer = false;
 	$notAPublicOfferDisclaimerDisplayMode = $viewData['airis-template-options']->get('virtuemartDisplayModeDisclaimerNotAPublicOffer', 'none');
 
-	if ($notAPublicOfferDisclaimerDisplayMode === 'always' || ($notAPublicOfferDisclaimerDisplayMode === 'catalog' && VmConfig::get('use_as_catalog', 0)))
-	{
+	if ($notAPublicOfferDisclaimerDisplayMode === 'always' || ($notAPublicOfferDisclaimerDisplayMode === 'catalog' && VmConfig::get('use_as_catalog', 0))) {
 		$haveDisclaimers = true;
 		$displayNotAPublicOfferDisclaimer = true;
 	}
@@ -35,13 +40,11 @@ if (isset($viewData['airis-template-options']))
 	$customDisclaimerLanguageConstantBase = 'TPL_AIRIS_COM_VIRTUEMART_PRODUCTDETAILS_DISCLAIMER_CUSTOM_';
 
 	// See if we have any custom disclaimers defined
-	if (!$haveDisclaimers) {
-		for ($currentCustomDisclaimerIndex = 1; $currentCustomDisclaimerIndex <= $maximumCustomDisclaimers; $currentCustomDisclaimerIndex++)
-		{
+	if ($haveDisclaimers === false) {
+		for ($currentCustomDisclaimerIndex = 1; $currentCustomDisclaimerIndex <= $maximumCustomDisclaimers; $currentCustomDisclaimerIndex++) {
 			$currentCustomDisclaimerLanguageConstant = $customDisclaimerLanguageConstantBase . $currentCustomDisclaimerIndex;
-		
-			if (Text::_($currentCustomDisclaimerLanguageConstant) !== $currentCustomDisclaimerLanguageConstant)
-			{
+
+			if (Text::_($currentCustomDisclaimerLanguageConstant) !== $currentCustomDisclaimerLanguageConstant) {
 				$haveDisclaimers = true;
 				break;
 			}
@@ -50,16 +53,28 @@ if (isset($viewData['airis-template-options']))
 }
 
 // TODO: Find out what this whole row heights thing actually does
-if (isset($viewData['rowHeights']))
-{
+if (isset($viewData['rowHeights'])) {
 	$rowHeights = $viewData['rowHeights'];
-} else
-{
+} else {
 	$rowHeights['customfields'] = true;
 }
 
-$positions = isset($viewData['position']) ? $viewData['position'] : 'addtocart';
-if (!is_array($positions)) $positions = array($positions);
+// Default custom field position this layout outputs along with its HTML-structure
+$positions = [
+	'addtocart',
+];
+
+// Alternative custom field positions are also accepted
+if (isset($viewData['position'])) {
+	if (is_array($viewData['position'])) {
+		$positions = $viewData['position'];
+	} else {
+		$positions = [
+			$viewData['position'],
+		];
+	}
+}
+
 ?>
 
 <div class="addtocart-area virtuemart-product-addtocart">
@@ -67,19 +82,38 @@ if (!is_array($positions)) $positions = array($positions);
 
 		<?php // TODO: Empty vm-customfields-wrap still gets into the output. Come up with a better conditional. ?>
 		<?php // TODO: Find out what this whole $rowHeights thing actually does ?>
-		<?php if (!empty($rowHeights['customfields'])) : ?>
+		<?php if (isset($rowHeights['customfields'])) : ?>
 			<div class="vm-customfields-wrap">
-				<?php foreach ($positions as $position) echo shopFunctionsF::renderVmSubLayout($sublayoutPrefix . 'customfields', array('product' => $product, 'position' => $position)); ?>
+				<?php
+					foreach ($positions as $position) {
+						echo shopFunctionsF::renderVmSubLayout(
+							"{$sublayoutPrefix}customfields",
+							[
+								'product' => $product,
+								'position' => $position,
+							],
+						);
+					}
+				?>
 			</div>
 		<?php endif; ?>
 
-		<?php if (!VmConfig::get('use_as_catalog', 0)) echo shopFunctionsF::renderVmSubLayout($sublayoutPrefix . 'addtocartbar', array('product' => $product, 'airis-template-options' => $viewData['airis-template-options'])); ?>
+		<?php
+			if (VmConfig::get('use_as_catalog', 0) === 0) {
+				echo shopFunctionsF::renderVmSubLayout(
+					"{$sublayoutPrefix}addtocartbar",
+					[
+						'product' => $product,
+						'airis-template-options' => $viewData['airis-template-options'],
+					],
+				);
+			}
+		?>
 
 		<input type="hidden" name="option" value="com_virtuemart">
 		<input type="hidden" name="view" value="cart">
-		<input type="hidden" name="virtuemart_product_id[]" value="<?php echo $product->virtuemart_product_id ?>">
-		<input type="hidden" name="pname" value="<?php echo $product->product_name ?>">
-		<input type="hidden" name="pid" value="<?php echo $product->virtuemart_product_id ?>">
+		<input type="hidden" name="pname" value="<?php echo $productName; ?>">
+		<input type="hidden" name="pid" value="<?php echo $product->virtuemart_product_id; ?>">
 
 		<?php $itemId = vRequest::getInt('Itemid', false); ?>
 		<?php if ($itemId) : ?>
@@ -90,23 +124,27 @@ if (!is_array($positions)) $positions = array($positions);
 
 	<?php // Custom Contact Us link with product id, name and sku exposed as data attributes. Optionally replaces the notify page link for out-of-stock products. ?>
 	<?php if (isset($viewData['airis-template-options']) && ($viewData['airis-template-options']->get('showVirtuemartProductLinkContactUs') || ($viewData['airis-template-options']->get('showVirtuemartProductLinkContactUs') && $product->show_notify && $viewData['airis-template-options']->get('replaceVirtuemartProductLinkNotifyWithContactUs')))) : ?>
-		
 		<?php
-
-			$contactUsLinkAttributes = array(
+			$contactUsLinkAttributes = [
 				'class' => 'virtuemart-product-link virtuemart-product-link-contact-us btn',
 				'rel' => 'nofollow',
 				'data-virtuemart-product-id' => $product->virtuemart_product_id,
-				'data-virtuemart-product-name' => htmlspecialchars(trim(vmText::_($product->product_name)), ENT_QUOTES, 'UTF-8')
-			);
+				'data-virtuemart-product-name' => $productName,
+			];
 
-			$productSku = htmlspecialchars(trim($product->product_sku), ENT_QUOTES, 'UTF-8');
-			if (!empty($productSku)) $contactUsLinkAttributes['data-virtuemart-product-sku'] = $productSku;
-
+			if ($productSku === '') {
+				$contactUsLinkAttributes['data-virtuemart-product-sku'] = $productSku;
+			}
 		?>
 
 		<div class="virtuemart-product-addtocart-links" data-nosnippet>
-			<?php echo HTMLHelper::link('#virtuemart-product-form-contact-us', vmText::_('TPL_AIRIS_COM_VIRTUEMART_PRODUCT_LINK_CONTACT_US_TEXT'), $contactUsLinkAttributes); ?>
+			<?php
+				echo HTMLHelper::link(
+					'#virtuemart-product-form-contact-us',
+					vmText::_('TPL_AIRIS_COM_VIRTUEMART_PRODUCT_LINK_CONTACT_US_TEXT'),
+					$contactUsLinkAttributes,
+				);
+			?>
 		</div>
 
 	<?php endif; ?>
