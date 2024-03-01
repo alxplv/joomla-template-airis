7a9b79ee6ee4b3701ff866f02524029ea3dae1c4
alxplv <polishchuk-alexander@mail.ru>
2024-03-01 22:28:33 +0600

    Use absolute path in Image class instance creation
    Also a general file refactoring.

--- html/layouts/joomla/html/image.php
+++ html/layouts/joomla/html/image.php
@@ -9,26 +9,32 @@ use Joomla\CMS\Image\Image;
 use Joomla\CMS\Log\Log;
 use Joomla\Utilities\ArrayHelper;
 
+// Skip empty images
+if (isset($displayData['src']) === false) {
+	return;
+}
+
+$imageAttributes = '';
+$pictureHtmlElementSources = [];
+
 // TODO: Include additonal sources from found modern image format files of the same name as the passed one
 // TODO: Test compatibility of used JS carousel libraries with <picture> tag
 
-// Src
-if (isset($displayData['src'])) {
-	// Remove extra substring Joomla's media manager which can be harmful to SEO
-	$itemImageDataCleaned = HTMLHelper::cleanImageURL($displayData['src']);
-	$displayData['src'] = $this->escape($itemImageDataCleaned->url);
-}
+// Remove extra substring Joomla's media manager which can be harmful to SEO
+$displayData['src'] = $this->escape(
+	HTMLHelper::cleanImageURL($displayData['src'])->url,
+);
 
 // Class
-if (isset($displayData['class'])) {
-	$displayData['class'] = 'img-fluid ' . $displayData['class'];
+if (isset($displayData['class']) && $displayData['class'] !== '') {
+	$displayData['class'] .= ' img-fluid';
 } else {
 	$displayData['class'] = 'img-fluid';
 }
 
 // Alt
 if (isset($displayData['alt'])) {
-	if ($displayData['alt'] === false) {
+	if ($displayData['alt'] === false || $displayData['alt'] === '') {
 		unset($displayData['alt']);
 	} else {
 		$displayData['alt'] = $this->escape($displayData['alt']);
@@ -36,30 +42,42 @@ if (isset($displayData['alt'])) {
 }
 
 // Title
-if (!empty($displayData['title'])) {
+if (isset($displayData['title']) && $displayData['title'] !== '') {
 	$displayData['title'] = $this->escape($displayData['title']);
 }
 
 // Width & Height
-if (empty($displayData['width']) || empty($displayData['height'])) {
+if (
+	(isset($displayData['width']) === false || $displayData['width'] === '')
+	|| (isset($displayData['height']) === false || $displayData['height'] === '')
+) {
 	try {
-		// Throws various exceptions if it is unable to create an image instance
-		$joomlaImageClassInstance = new Image($displayData['src']);
+		// NOTE: Have to use an absolute path in layouts unlike when working with views
+		$imagePathAbsolute = join(
+			DIRECTORY_SEPARATOR,
+			[
+				JPATH_BASE,
+				$displayData['src'],
+			],
+		);
 
-		// In case if path string points to a non-existing file
+		// Throws various exceptions if it is unable to create an image instance
+		$joomlaImageClassInstance = new Image($imagePathAbsolute);
+
+		// getWidth() and getHeight() too throw exceptions for nonexistent images
 		if ($joomlaImageClassInstance->isLoaded()) {
-			if (empty($displayData['width'])) {
+			if (isset($displayData['width']) === false) {
 				$displayData['width'] = $joomlaImageClassInstance->getWidth();
 			}
 
-			if (empty($displayData['height'])) {
+			if (isset($displayData['height']) === false) {
 				$displayData['height'] = $joomlaImageClassInstance->getHeight();
 			}
 		}
 	} catch (Exception $joomlaImageClassInstanceCreationException) {
 		if (JDEBUG) {
 			Log::add(
-				"Unable to acquire image dimensions of file \"{$displayData['src']}\". " . $joomlaImageClassInstanceCreationException->getMessage(),
+				"Unable to acquire image dimensions of file \"$imagePathAbsolute\". " . $joomlaImageClassInstanceCreationException->getMessage(),
 				Log::DEBUG,
 				'templates-airis-html-layouts-joomla-html-image',
 			);
@@ -68,7 +86,7 @@ if (empty($displayData['width']) || empty($displayData['height'])) {
 }
 
 // Loading
-if (empty($displayData['loading'])) {
+if (isset($displayData['loading']) === false) {
 	$displayData['loading'] = 'lazy';
 }
 
@@ -76,19 +94,22 @@ if (empty($displayData['loading'])) {
 	'avif',
 	'heic',
 	'heif',
-	'webp',
-];
+	'webp', // As of Joomla! 5.0, only this format is supported by the Image class
+]; */
 
 // Should probably just try to pass each file variant path to Image class contrutor or find a Joomla!'s proper equivalent of file_exists();
-foreach ($supportedPictureElementSourceTypeExtensions as $supportedPictureElementSourceTypeExtension) {
+/* foreach ($supportedPictureElementSourceTypeExtensions as $supportedPictureElementSourceTypeExtension) {
 	pathinfo($item->imageSrc, PATHINFO_)
 
 	if (file_exists()) {}
 } */
 
-$pictureSources = '';
+?>
 
-// Implode image attributes into a final string
-$imageAttributes = ArrayHelper::toString($displayData);
+<picture>
+	<?php if (count($pictureHtmlElementSources)) : ?>
 
-echo "<picture>$pictureSources<img $imageAttributes></picture>";
+	<?php endif; ?>
+
+	<img <?= ArrayHelper::toString($displayData); ?>>
+</picture>
