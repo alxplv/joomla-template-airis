c27e1afc0c552fb887bcd9bab883029d6e6c203a
alxplv <polishchuk-alexander@mail.ru>
2026-05-15 08:29:49 +0600

    Add the "use strict" directive to all template JS.

--- js/user.js
+++ js/user.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // User JavaScript
 // Please don't forget to enable the use jQuery in template options if you intent to write jQuery-targeted code such as the blocks commented out below
 /* jQuery(function () {
